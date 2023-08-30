3850b9ef12eb4bce71454090a1d32628d9a90fde
Lucas Cantarelli <lucascantarellijpa@gmail.com>
2023-08-29 23:55:29 -0300

    Criando o logger.

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -13,3 +13,15 @@ async def test_health_check():
 
     if response.json() != {"status_code": 200, "data": "healthy"}:
         raise ValueError(f"Response: {response.json()}")
+
+
+@pytest.mark.anyio
+async def test_main():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get("/")
+
+    if response.status != 200:
+        raise ValueError(f"HTTP Status: {response.status}")
+
+    if response.json() != {"status_code": 200, "data": "OK"}:
+        raise ValueError(f"Response: {response.json()}")
