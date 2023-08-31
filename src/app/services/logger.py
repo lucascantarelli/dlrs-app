e4f435f03ddc4bb04af43a118b370674858b5f83
Lucas Cantarelli <lucascantarellijpa@gmail.com>
2023-08-31 18:11:36 -0300

    Log pelo uvicorn.

--- src/app/services/logger.py
+++ src/app/services/logger.py
@@ -1,6 +1,7 @@
 import logging
+from logging.config import dictConfig
 
-from src.app.settings import Settings
+from app.settings import Settings
 
 
 class Logger:
@@ -9,12 +10,53 @@ class Logger:
 
     def __init__(self, name: str):
         # Configura o logging.
-        logging.basicConfig(
-            format=self._settings.APP_LOG_FORMAT,
-            level=self._settings.APP_LOG_LEVEL,
-        )
+        self._log_config: dict = {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "access": {
+                    "()": "uvicorn.logging.AccessFormatter",
+                    "fmt": "%(levelprefix)s %(asctime)s - %(client_addr)s"
+                    + '- "%(request_line)s" %(status_code)s',
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                    "use_colors": True,
+                },
+                "default": {
+                    "()": "uvicorn.logging.DefaultFormatter",
+                    "fmt": "%(levelprefix)s %(asctime)s - %(message)s",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                    "use_colors": True,
+                },
+            },
+            "handlers": {
+                "access": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "access",
+                    "stream": "ext://sys.stdout",
+                },
+                "default": {
+                    "formatter": "default",
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://sys.stderr",
+                },
+            },
+            "loggers": {
+                name: {"handlers": ["default"], "level": "DEBUG", "propagate": False},
+                "uvicorn": {
+                    "handlers": ["default"],
+                    "level": "DEBUG",
+                    "propagate": True,
+                },
+                "uvicorn.access": {
+                    "handlers": ["access"],
+                    "level": "INFO",
+                    "propagate": False,
+                },
+                "uvicorn.error": {"level": "INFO", "propagate": False},
+            },
+        }
 
-        # Seta o logging.
+        dictConfig(self._log_config)
         self._log = logging.getLogger(name)
 
     async def info(self, message: str, *args):
--- src/app/settings.py
+++ src/app/settings.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     APP_ENV: str = os.environ.get("APP_ENV", "development")
 
     # Logger settings
+    APP_LOG_FILE: str = os.environ.get("APP_LOG_FILE", "logging.conf")
     APP_LOG_LEVEL: str = os.environ.get("APP_LOG_LEVEL", "DEBUG")
     APP_LOG_FORMAT: str = os.environ.get(
         "APP_LOG_FORMAT",
